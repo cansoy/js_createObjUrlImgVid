830e85b3c52fda821308e168b3da258dbd2c61ac
muhammed.cansoy <cansoymuhammed@hotmail.com>
2022-10-18 21:25:20 +0300

    input file & fetch blob difference

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const btnImg=document.querySelector('#img')
 const btnVideo=document.querySelector('#video')
 const divimg=document.querySelector('#divimg')
 const divvideo=document.querySelector('#divvideo')
+const imagefile=document.querySelector('#img-file')
+const videofile=document.querySelector('#video-file')
+const containerImage=document.querySelector('.container-image')
+const containerVideo=document.querySelector('.container-video')
 
 const imgObjUrl=async(e)=>{
     const response=await fetch('./db/image.jpg')
@@ -27,3 +31,33 @@ const videoObjUrl=async(e)=>{
 }
 btnImg.addEventListener('click',imgObjUrl)
 btnVideo.addEventListener('click',videoObjUrl)
+
+imagefile.addEventListener('change',(e)=>{
+    const file=imagefile.files[0]
+    const filereader=new FileReader()
+    filereader.readAsDataURL(file)
+    filereader.onload=(ev)=>{
+        const result=filereader.result
+        const image=new Image(200,200)
+        image.src=result
+        containerImage.appendChild(image)
+    }
+})
+
+videofile.addEventListener('change',(e)=>{
+    // const file=videofile.files[0]
+    const file=e.target.files[0]
+    const filereader=new FileReader()
+    filereader.readAsDataURL(file)
+    filereader.onload=(ev)=>{
+        const result=filereader.result
+        const video=document.createElement('video')
+        video.width=200
+        video.height=200
+        video.controls=true
+        video.src=result
+        containerVideo.appendChild(video)
+    }
+
+    
+})
